935d6d34e626f38b9066de32c84a2589c1f6b26d
joyce.sousa <joyce.sousa@LAB2-DCX-07.labs.dcx>
2020-02-13 07:46:01 -0300

    gzuis

---
+++ EngenSoft/src/Joyce.java
@@ -0,0 +1,14 @@
+
+public class Joyce {
+	public static void main(String[] args) {
+		int num1 = 4;
+		int num2 = 8;
+		int num3 = soma(num1,num2);
+		System.out.println(num3);
+	}
+	
+	public static int soma(int num1, int num2) {
+		return num1+num2;
+	}
+
+}
